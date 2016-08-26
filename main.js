6baf69e63fd3977c3786e9f845b38974250a46b6
Jonatas Freitas <jonatasfreitasv@gmail.com>
2016-08-26 10:37:41 -0300

    Initial commit

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL('https://www.mindmeister.com/pt/account/login?product=1')
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
